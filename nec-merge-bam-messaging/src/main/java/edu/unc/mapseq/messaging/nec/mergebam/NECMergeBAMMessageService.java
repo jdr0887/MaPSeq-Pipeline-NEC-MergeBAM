a900ae3975c07338a3cce82561f43d35a69c7cb8
Jason Reilly <jdr0887@renci.org>
2014-06-06 15:57:38 -0400

    initial commit

---
+++ nec-merge-bam-messaging/src/main/java/edu/unc/mapseq/messaging/nec/mergebam/NECMergeBAMMessageService.java
@@ -0,0 +1,75 @@
+package edu.unc.mapseq.messaging.nec.mergebam;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NECMergeBAMMessageService {
+
+    private final Logger logger = LoggerFactory.getLogger(NECMergeBAMMessageService.class);
+
+    private Connection connection;
+
+    private Session session;
+
+    private ConnectionFactory connectionFactory;
+
+    private NECMergeBAMMessageListener messageListener;
+
+    private String destinationName;
+
+    public NECMergeBAMMessageService() {
+        super();
+    }
+
+    public void start() throws Exception {
+        logger.info("ENTERING start()");
+        this.connection = connectionFactory.createConnection();
+        this.session = this.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = this.session.createQueue(this.destinationName);
+        MessageConsumer consumer = this.session.createConsumer(destination);
+        consumer.setMessageListener(messageListener);
+        this.connection.start();
+    }
+
+    public void stop() throws Exception {
+        logger.info("ENTERING stop()");
+        if (this.session != null) {
+            this.session.close();
+        }
+        if (this.connection != null) {
+            this.connection.stop();
+            this.connection.close();
+        }
+    }
+
+    public ConnectionFactory getConnectionFactory() {
+        return connectionFactory;
+    }
+
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    public NECMergeBAMMessageListener getMessageListener() {
+        return messageListener;
+    }
+
+    public void setMessageListener(NECMergeBAMMessageListener messageListener) {
+        this.messageListener = messageListener;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+}
